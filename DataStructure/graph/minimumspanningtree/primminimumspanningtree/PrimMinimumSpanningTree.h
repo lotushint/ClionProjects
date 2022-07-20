78b6cf2b46e1009b69a55e656843cb782f910cf9
lotushint <2870999158@qq.com>
2022-07-20 19:37:11 +0800

    2022-7-20-Added Prim minimum spanning tree algorithm (adjacency matrix)

---
+++ DataStructure/graph/minimumspanningtree/primminimumspanningtree/PrimMinimumSpanningTree.h
@@ -0,0 +1,87 @@
+//
+// Created by lotushint on 2022/7/19.
+// Prim 最小生成树算法
+//
+
+#ifndef DATASTRUCTURE_PRIMMINIMUMSPANNINGTREE_H
+#define DATASTRUCTURE_PRIMMINIMUMSPANNINGTREE_H
+
+#include "../../adjacency/adjacencymatrix/AdjacencyMatrix.h"
+
+/**
+ * 用于保存最小生成树的边类型定义
+ */
+typedef struct edgeData {
+    /**
+     * 边顶点序号
+     */
+    int begin, end;
+    /**
+     * 边长
+     */
+    int length;
+} edge;
+
+/**
+ * prim算法构造最小生成树
+ * @param matrixGraph 图的邻接矩阵
+ * @param treeEdge 边向量
+ */
+void primMinimumSpanningTree(AdjacencyMatrixGraph matrixGraph, edge treeEdge[M - 1]) {
+    edge x;
+    int v, k, min, s, j, d;
+    /*
+     * 建立初始入选点，并初始化生成树边集 treeEdge
+     */
+    for (v = 1; v <= matrixGraph.vertexNumber - 1; v++) {
+        /*
+         * 此处从顶点 v0 开始求最小生成树,将 V0——Vv(v = 1,2,3,...,vertexNumber-1) 边加入 treeEdge
+         */
+        treeEdge[v - 1].begin = 0;
+        treeEdge[v - 1].end = v;
+        treeEdge[v - 1].length = matrixGraph.edge[0][v];
+    }
+    /*
+     * 依次求当前（第 k 条）最小两栖边，并加入 treeEdge
+     */
+    for (k = 0; k <= matrixGraph.vertexNumber - 3; ++k) {
+        min = treeEdge[k].length;
+        s = k;
+        for (j = k + 1; j <= matrixGraph.vertexNumber - 2; ++j) {
+            if (treeEdge[j].length < min) {
+                min = treeEdge[j].length;
+                s = j;
+            }
+        }
+        //入选顶点为 v
+        v = treeEdge[s].end;
+
+        /*
+         * 通过交换，将当前最小边加入 treeEdge 中
+         */
+        x = treeEdge[s];
+        treeEdge[s] = treeEdge[k];
+        treeEdge[k] = x;
+        /**
+         * 由于新顶点 v 的加入，修改两栖边的基本信息
+         */
+        for (j = k + 1; j <= matrixGraph.vertexNumber - 2; ++j) {
+            d = matrixGraph.edge[v][treeEdge[j].end];
+            if (d < treeEdge[j].length) {
+                treeEdge[j].length = d;
+                treeEdge[j].begin = v;
+            }
+        }
+    }
+    /*
+     * 输出最小生成树
+     */
+    printf("\n The minimum cost spanning tree is:\n");
+    for (j = 0; j <= matrixGraph.vertexNumber - 2; ++j) {
+        printf("\n%c---%c %d\n", matrixGraph.vertex[treeEdge[j].begin], matrixGraph.vertex[treeEdge[j].end],
+               treeEdge[j].length);
+    }
+    printf("\nThe root of it is %c\n", matrixGraph.vertex[0]);
+}
+
+#endif //DATASTRUCTURE_PRIMMINIMUMSPANNINGTREE_H
--- DataStructure/graph/test/PrimMinimumSpanningTree.c
+++ DataStructure/graph/test/PrimMinimumSpanningTree.c
@@ -1,4 +1,35 @@
 //
 // Created by lotushint on 2022/7/19.
 // Prim最小生成树算法
+// 输入：
+//      ../graph/minimumspanningtree/files/graph.txt
+// 输出：
+//      The minimum cost spanning tree is:
 //
+//      0---1 10
+//
+//      1---3 5
+//
+//      1---5 6
+//
+//      1---2 7
+//
+//      5---4 10
+//
+//      The root of it is 0
+#include "../minimumspanningtree/primminimumspanningtree/PrimMinimumSpanningTree.h"
+
+int main() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    AdjacencyMatrixGraph matrixGraph;
+    /**
+     * 用于存放最小生成数的 M - 1 条边
+     */
+    edge treeEdge[M - 1];
+    char filename[20];
+    printf("Please input filename of graph:\n");
+    gets(filename);
+    createAdjacencyMatrixGraph(&matrixGraph, filename, 0);
+    primMinimumSpanningTree(matrixGraph, treeEdge);
+    return 0;
+}
