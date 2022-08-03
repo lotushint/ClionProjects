2a4874c8a1420fa58130018402d34ff36b6c8d7a
lotushint <2870999158@qq.com>
2022-08-03 19:04:15 +0800

    2022-8-3-Completed block search (index sequential search)

--- DataStructure/retrieve/indexordersearch/IndexOrderSearch.h
+++ DataStructure/retrieve/indexordersearch/IndexOrderSearch.h
@@ -25,8 +25,41 @@ typedef struct {
  * @return 若查找成功，返回其所在位置；否则，返回 -1
  */
 int indexSequentialSearch(SequenceList sequenceList, IndexNode indexNode[], int indexListLength, dataType key) {
-    int i = 0, j, last;
+    /**
+     * 关键字所在块的索引
+     */
+    int blockIndex = 0;
+    int j;
+    //找到关键字所在块
+    while (blockIndex < indexListLength && key > indexNode[blockIndex].key) {
+        blockIndex++;
+    }
+    //没找到关键字所在块
+    if (blockIndex >= indexListLength) {
+        return -1;
+    } else {
+        /*
+         * 在顺序表中顺序检索
+         */
+        if (blockIndex == indexListLength - 1) {
+            j = sequenceList.length - 1;
+        } else {
+            // j 初始时指向本块的最后一个结点（索引表中下一个元素的地址-1 即是该块最后一个元素的索引）
+            j = indexNode[blockIndex + 1].address - 1;
+        }
 
+        //从后向前逐个查找
+        while (j >= indexNode[blockIndex].address && key != sequenceList.data[j]) {
+            j--;
+        }
+
+        //没找到
+        if (j < indexNode[blockIndex].address) {
+            return -1;
+        } else {
+            return j;
+        }
+    }
 }
 
 #endif //DATASTRUCTURE_INDEXORDERSEARCH_H
--- DataStructure/retrieve/test/IndexOrderSearch.c
+++ DataStructure/retrieve/test/IndexOrderSearch.c
@@ -4,3 +4,45 @@
 //
 
 #include "../indexordersearch/IndexOrderSearch.h"
+
+int main() {
+    SequenceList sequenceList;
+    IndexNode indexNode[3];
+
+    sequenceList.data[0] = 20;
+    sequenceList.data[1] = 4;
+    sequenceList.data[2] = 25;
+    sequenceList.data[3] = 17;
+    sequenceList.data[4] = 3;
+    sequenceList.data[5] = 12;
+    sequenceList.data[6] = 6;
+    sequenceList.data[7] = 40;
+    sequenceList.data[8] = 30;
+    sequenceList.data[9] = 27;
+    sequenceList.data[10] = 55;
+    sequenceList.data[11] = 48;
+    sequenceList.data[12] = 35;
+    sequenceList.data[13] = 70;
+    sequenceList.data[14] = 66;
+    sequenceList.data[15] = 90;
+    sequenceList.data[16] = 60;
+    sequenceList.data[17] = 86;
+    sequenceList.data[18] = 73;
+    sequenceList.data[19] = 69;
+    sequenceList.length = 20;
+
+    indexNode[0].address = 0;
+    indexNode[0].key = 25;
+
+    indexNode[1].address = 7;
+    indexNode[1].key = 55;
+
+    indexNode[2].address = 13;
+    indexNode[2].key = 90;
+
+
+    printf(" %2d ", indexSequentialSearch(sequenceList, indexNode, 3, 27));
+    printf(" %2d ", indexSequentialSearch(sequenceList, indexNode, 3, 2789));
+    printf(" %2d ", indexSequentialSearch(sequenceList, indexNode, 3, 69));
+    return 0;
+}
